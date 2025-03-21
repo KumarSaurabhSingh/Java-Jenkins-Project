d9aef41b18a14129ce4bc6a471fd5d4a3c485be3
KumarSaurabhSingh <iamkuwarsaurabhkumarsingh@gmail.com>
2025-03-21 16:08:01 +0530

    Ne jenkins

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hey!! I am under Java Jenkins Project");
+        System.out.println("Sucessfully Executed!!!");
+    }
+}
